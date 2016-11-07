f03d53aa0ce09911076b050580527e017f465b00
Sylyu <sylyu>
2016-11-07 02:05:59 +0000

    set up the welcome page with static content on it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 NomsterQuiz1::Application.routes.draw do
+  root 'mypages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
